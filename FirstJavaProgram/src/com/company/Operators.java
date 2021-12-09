f9ba9f7a60f7b47d9ba4b198bcb582352c3715fd
RoopsBhasima7 <rupeshbhasima@gmail.com>
2021-12-09 11:38:27 +0545

    Basics of Java

---
+++ FirstJavaProgram/src/com/company/Operators.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Operators {
+    public static void main(String[] args) {
+        int b = 9;
+        b *= 4;
+        System.out.println(b);
+//        System.out.println(7==7);
+        System.out.println(43>7 && 43>9);
+    }
+}
